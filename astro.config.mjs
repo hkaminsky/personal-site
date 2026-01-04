d1f1efdc900ddc412b6b8345aec7e20bb09a7098
hkaminsky <hdkamin@gmail.com>
2026-01-03 21:25:34 -0500

    Env variables, footer updates to main

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const isPreviewBuild = process.env.PREVIEW_BUILD === 'true';
+
+const getBasePath = () => {
+  if (isPreviewBuild) return '/personal-site/preview/';
+  if (isGitHubPages) return '/personal-site';
+  return '/';
+};
+
 export default defineConfig({
   site: 'https://hkaminsky.github.io',
-  base: '/personal-site',
+  base: getBasePath(),
   integrations: [mdx(), sitemap(), tailwind()],
 });
