0c5580ce37af576b7c1e179269c1202ede6fe8dc
hkaminsky <hdkamin@gmail.com>
2026-01-03 20:34:29 -0500

    Updates to env variables, and footer icon update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
   site: 'https://hkaminsky.github.io',
-  base: '/personal-site',
+  base: isGitHubPages ? '/personal-site' : '/',
   integrations: [mdx(), sitemap(), tailwind()],
 });
